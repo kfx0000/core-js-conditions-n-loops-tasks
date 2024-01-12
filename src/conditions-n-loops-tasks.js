3239ffdd1202076c3e43b60ced320798cd2ac1bd
Andy <kfx0000@gmail.com>
2024-01-12 15:30:09 +0300

    feat: add 17 solutions

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -409,8 +409,15 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let res = str;
+  for (let i = 0; i < iterations; i += 1) {
+    const arr = ['', ''];
+    for (let j = 0; j < res.length; j += 1) arr[j % 2] += res[j];
+    res = arr[0] + arr[1];
+    if (res === str) return shuffleChar(str, iterations % (i + 1));
+  }
+  return res;
 }
 
 /**
